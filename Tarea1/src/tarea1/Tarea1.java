7c5f3a62624ce4a04c9385f3b38f7f6dc0d43c65
PabloSanhueza1 <Sanhueza.pem@gmail.com>
2022-10-05 20:25:54 -0300

    orden detalle articulo

--- Tarea1/src/tarea1/Tarea1.java
+++ Tarea1/src/tarea1/Tarea1.java
@@ -7,3 +7,63 @@ public class Tarea1 {
     }
     
 }
+
+class OrdenCompra {
+
+    private int fecha;
+    private String estado;
+
+    DocTributario doctributario = new DocTriburario(String numero , String rut , int fecha
+
+    ); 
+    
+    public OrdenCompra(int fecha, String estado) {
+        this.fecha = fecha;
+        this.estado = estado;
+    }
+
+    public void calcPrecioSinIVA() {
+    }
+
+    public void calcIVA() {
+    }
+
+    public void calcPrecio() {
+    }
+
+    public void calcPeso() {
+    }
+}
+
+class DetalleOrden {
+
+    private int cantidad;
+
+    public DetalleOrden(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void calcPrecioSinIVA() {
+    }
+
+    public void calcIVA() {
+    }
+
+    public void calcPeso() {
+    }
+}
+
+class Articulo {
+
+    private float peso;
+    private String nombre;
+    private String descripcion;
+    private String precio;
+
+    public Articulo(float peso, String nombre, String descripcion, String precio) {
+        this.peso = peso;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+}
