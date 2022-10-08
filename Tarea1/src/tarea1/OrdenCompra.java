13e6c5d350b502f6f7e3e670648fbb03e4ff050f
Joaquin <joaquin.san.vargas19@gmail.com>
2022-10-07 23:10:33 -0300

    Formateo del codigo

--- Tarea1/src/tarea1/OrdenCompra.java
+++ Tarea1/src/tarea1/OrdenCompra.java
@@ -8,28 +8,29 @@ class OrdenCompra {
     private String estado;
     private Pedido pedido;
     protected LocalDate fecha;
-        
+
     public OrdenCompra(String estado, Pedido pedido, Cliente cliente, int doc) {
         this.fecha = LocalDate.now();
         this.estado = estado;
         this.pedido = pedido;
         this.fecha = LocalDate.now();
         this.cliente = cliente;
-        
+
         DocTributario docTributario = null;
         switch (doc) {
             case 1:
-                docTributario = new Boleta("111",cliente.getRut(),fecha);
+                docTributario = new Boleta("111", cliente.getRut(), fecha);
                 break;
             case 2:
-                docTributario = new Factura("111",cliente.getRut(),fecha);
+                docTributario = new Factura("111", cliente.getRut(), fecha);
                 break;
-        }   
+        }
     }
-    
-     //DocTributario docTributario = new DocTributario(String numero, String rut, int fecha); 
+
+    // DocTributario docTributario = new DocTributario(String numero, String rut,
+    // int fecha);
     DetalleOrden detalleOrden = new DetalleOrden(pedido);
-    
+
     public float calcPrecioSinIVA() {
         return detalleOrden.calcPrecioSinIVA(pedido);
     }
@@ -45,16 +46,18 @@ class OrdenCompra {
     public float calcPeso() {
         return detalleOrden.calcPeso(pedido);
     }
+
     public LocalDate getFecha() {
         return fecha;
     }
-    public Cliente getCliente(){
+
+    public Cliente getCliente() {
         return cliente;
     }
-    public String ToString(){
+
+    public String ToString() {
         String string = cliente.toString();
         string += detalleOrden.ToString();
         return string;
     }
 }
-
